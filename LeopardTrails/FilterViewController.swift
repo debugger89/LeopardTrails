764b0671046dcf2d2608fd7388142cc83b3dd627
Chathura Dushmantha <dushmantha.1989@gmail.com>
2019-07-04 22:44:55 +0530

    added new files

---
+++ LeopardTrails/FilterViewController.swift
@@ -0,0 +1,13 @@
+//
+//  FilterViewController.swift
+//  LeopardTrails
+//
+//  Created by Chathura Dushmantha on 7/3/19.
+//  Copyright © 2019 debugger. All rights reserved.
+//
+
+import UIKit
+
+class FilterViewController: NSObject {
+
+}
